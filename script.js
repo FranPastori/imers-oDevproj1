d46684f1e2e3089caa7c8cefa9d80dca43836196
FranPastori <franciele_pastori@hotmail.com>
2022-09-07 19:07:04 -0300

    Calcula fahrenheit

--- script.js
+++ script.js
@@ -1,5 +1,19 @@
 function ConverterFahrenheit() {
-var valorCelciusF = 0;
-
-
+var valorCelciusF = (document.getElementById("valor")).value;
+var valorCelciuNumerico = parseFloat(valorCelciusF)
+var valorEmFahrenheit = (valorCelciuNumerico*1.8)+32
+console.log (valorEmFahrenheit);
+var elementoValorConvertido = document.getElementById ("valorConvertidoFahrenheit");
+var valorConvertido = valorEmFahrenheit + "°F";
+elementoValorConvertido.innerHTML = valorConvertido;
 }
+
+function ConverterKelvin() {
+    var valorCelciusF = (document.getElementById("valor")).value;
+    var valorCelciuNumerico = parseFloat(valorCelciusF)
+    var valorEmKelvin = (valorCelciuNumerico+273)
+    console.log (valorEmKelvin);
+    var elementoValorConvertido = document.getElementById ("valorConvertidokevin");
+    var valorConvertido = valorEmKelvin + "°F";
+    elementoValorConvertido.innerHTML = valorConvertido;
+    }
